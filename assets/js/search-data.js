d714fbe2b20f4185c1a193ccba099c66b37de749
abolfazlshahsavaryyy <abolfazlshahsavaryyy@gmail.com>
2025-09-14 15:13:23 +0000

    Deploying to gh-pages from @ abolfazlshahsavaryyy/abolfazlshahsavaryyy.github.io@e7e3f3d5dbca59e0bdfae8ebde3484c32cae8ed3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -416,9 +416,9 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-citizen-journal",
+          title: 'Citizen Journal',
+          description: "Twitter-like project with Django, FastAPI, ML models, Celery, RabbitMQ &amp; GraphQL",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
             },},{id: "projects-project-2",
